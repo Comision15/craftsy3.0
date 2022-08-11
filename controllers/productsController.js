8d457272ca1419913478aa9c4970d2f0a475c4d6
EricM76 <menaericdaniel@gmail.com>
2022-08-10 21:01:24 -0300

    add R for CRUD

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,25 @@
+const products = require('../data/productsModule').loadProducts();
+
 module.exports = {
     detail : (req,res) => {
-        return res.render('productDetail')
+
+        const product = products.find(product => product.id === +req.params.id);
+
+        return res.render('productDetail', {
+            product
+        })
+    },
+    filter : (req,res) => {
+        const productsFilter = products.filter(product => product.section === req.query.section)
+        return res.render('products', {
+            products : productsFilter
+        })
+    },
+    search : (req,res) => {
+        const result = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()))
+        return res.render('products', {
+            products : result,
+            keywords : req.query.keywords
+        })
     }
 }
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {detail} = require('../controllers/productsController');
+const {detail, filter, search} = require('../controllers/productsController');
 
 /* /products */
 
 router
-    .get('/detail',detail)
+    .get('/detail/:id',detail)
+    .get('/filter',filter)
+    .get('/search',search)
 
 
 
